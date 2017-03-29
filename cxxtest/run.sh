302b3573bb2c0fd35286beb083362dee3a083ec0
Alzater <alzater@mail.ru>
2017-03-30 02:10:29 +0300

    add test for random_access_iterator

--- cxxtest/run.sh
+++ cxxtest/run.sh
@@ -3,6 +3,6 @@
 mkdir build -p
 cd build
 
-cxxtestgen --error-printer -o runner.cpp ../test_array.h ../test_print.h ../test_random_access_iterator.h
+cxxtestgen --error-printer -o runner.cpp ../test_array.h ../test_random_access_iterator.h
 g++ -std=c++14 -o runner -I"../../../cxxtest-4.4" runner.cpp
 ./runner
