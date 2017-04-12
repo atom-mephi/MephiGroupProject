096d5cf6e4a0e9089d60ddc8eae8329e7f8161c5
Alzater <alzater@mail.ru>
2017-04-12 19:31:04 +0300

    add unique_ptr with tests

--- cxxtest/run.sh
+++ cxxtest/run.sh
@@ -3,6 +3,6 @@
 mkdir build -p
 cd build
 
-cxxtestgen --error-printer -o runner.cpp ../test_array.h ../test_random_access_iterator.h
+cxxtestgen --error-printer -o runner.cpp ../test_array.h ../test_random_access_iterator.h ../test_unique_ptr.h
 g++ -std=c++14 -o runner -I"../../../cxxtest-4.4" runner.cpp
 ./runner
